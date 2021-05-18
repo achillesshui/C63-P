ed368358dca5581bd816be83b02c24247b057875
achillesshui <56407377+achillesshui@users.noreply.github.com>
2021-05-18 10:23:53 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      displayText: '',
+    };
+  }
+
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <HomeScreen />
+        </View>
+      </SafeAreaProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8'
+  },
+});
